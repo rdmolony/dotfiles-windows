014a4c5b3d4711ceca124abebf705db20ad40112
Rowan Molony <rowanmolony@gmail.com>
2021-10-20 17:47:45 +0100

    Add powershell-git-aliases
    See https://github.com/gluons/powershell-git-aliases

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -6,6 +6,7 @@ Set-PoshPrompt -Theme .\.rdmolony.omp.json
 
 ## aliases
 . .\conda-aliases.ps1
+Import-Module git-aliases -DisableNameChecking
 
 # enable tab completion
 Set-PSReadlineKeyHandler -Key Tab -Function Complete
